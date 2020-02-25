a746ab0f348efd3fdbcea53058907ccc76e6ab1c
Sarah Naas <sjnaas@gmail.com>
2020-02-25 11:46:58 -0800

    established connection to googlebooks API and created a route to a second page with React

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import axios from "axios";
+import Saved from "./components/Saved/Saved";
 
 function App() {
   useEffect(() => {
-    axios.get("/example").then(response => console.log(response));
+    axios
+      .request({
+        method: "GET",
+        url: "https://www.googleapis.com/books/v1/volumes?q=" + "harry+potter"
+      })
+      .then(response => console.log(response));
   }, []);
   return (
     <div className="App">
-      <h1>Heyyy</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/saved">
+            <Saved />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
