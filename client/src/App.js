5915b7c3c10a75c45a62fb090160c218f8f53213
Sarah Naas <sjnaas@gmail.com>
2020-04-03 15:54:46 -0700

    testing heroku again

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-
+import Form from "./components/Form/Form";
 import saved from "./pages/saved";
 import home from "./pages/home";
 import Saved from "./components/Saved/Saved";
