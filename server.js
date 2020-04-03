848ef8ec96d05cd2f8c4946d21b75aeb12a3981e
Sarah Naas <sjnaas@gmail.com>
2020-04-03 12:08:44 -0700

    testing heroku bug

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
-const routes = require("./routes/apiRoutes");
+//const routes = require("./routes/apiRoutes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
+//app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
