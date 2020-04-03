7e101ee3986e384395b7692373b3aec14aaad1e0
Sarah Naas <sjnaas@gmail.com>
2020-04-03 15:45:11 -0700

    Testing Heroku issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-// import axios from "axios";
-// import Saved from "./components/Saved/Saved";
-// import Form from "./components/Form/Form";
 import Navbar from "./components/Navbar/Navbar";
 
 import saved from "./pages/saved";
 import home from "./pages/home";
+import Saved from "./components/Saved/Saved";
 
 function App() {
   return (
@@ -15,8 +13,12 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={home} />
-          <Route exact path="/mysaved" component={saved} />
+          <Route exact path="/" component={home}>
+            <Form />
+          </Route>
+          <Route exact path="/mysaved" component={saved}>
+            <Saved />
+          </Route>
         </Switch>
       </Router>
     </div>
