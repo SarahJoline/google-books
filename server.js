551eb59458ce5bd1794afd8e03320d45a7b06111
Sarah Naas <sjnaas@gmail.com>
2020-04-03 20:35:41 -0700

    working on heroku bug

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  // app.get("/", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 mongoose
