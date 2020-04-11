28c79a4da2e8455634a0f76dfc76d325c0362a28
Sarah Naas <sjnaas@gmail.com>
2020-04-11 12:28:16 -0700

    testing H

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 mongoose
   .connect(MONGO_URI, {
@@ -26,6 +19,13 @@ mongoose
 
 const apiRoutes = require("./routes/apiRoutes");
 app.use("/api", apiRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`listening at http://localhost:${PORT}`);
