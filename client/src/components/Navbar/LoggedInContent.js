2cf6b0cb409d554dc5bcb2631e00a3bea1522a0a
Sarah Naas <sjnaas@gmail.com>
2024-02-09 00:14:46 +0100

    button

--- client/src/components/Navbar/LoggedInContent.js
+++ client/src/components/Navbar/LoggedInContent.js
@@ -74,22 +74,33 @@ function LoggedInContent(props) {
     <div className="btn-div">
       {lending ? <BorrowButton /> : <LendButton />}
       <div className="user-info">
-        <div
-          className="user-email"
+        <button
+          className="user-email-button"
           onClick={(e) => {
             handleClick(e);
           }}
         >
-          <header> {userData ? userData.email : " "}</header>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="24"
-            viewBox="0 -960 960 960"
-            width="24"
-          >
-            <path d="M480-345 240-585l56-56 184 184 184-184 56 56-240 240Z" />
-          </svg>
-        </div>
+          {userData ? userData.email : " "}
+          {open ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
+            >
+              <path d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
+            >
+              <path d="M480-345 240-585l56-56 184 184 184-184 56 56-240 240Z" />
+            </svg>
+          )}
+        </button>
         <div className="user-book-num">
           {/* <img src="/assets/Vector.svg" alt="X" /> */}
           LENDING {myBooks ? myBooks.length : "0"} BOOKS
@@ -106,6 +117,14 @@ function LoggedInContent(props) {
             horizontal: "right",
           }}
         >
+          <button
+            className="messages-button"
+            onClick={(e) => {
+              console.log("this will open messages tab");
+            }}
+          >
+            Messages
+          </button>
           <button
             className="logoutBtn"
             onClick={(e) => {
