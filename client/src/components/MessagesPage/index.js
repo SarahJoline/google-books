789170231dc9b22d4a3408e7de086f06eb56353c
Sarah Naas <sjnaas@gmail.com>
2024-02-09 02:16:57 +0100

    added get function

--- client/src/components/MessagesPage/index.js
+++ client/src/components/MessagesPage/index.js
@@ -1,7 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AuthHelperMethods from "../../helpers/AuthHelperMethods";
 import "./index.css";
 
 function MessagesPage() {
+  const [conversations, setConversations] = useState([]);
+  const userInfo = AuthHelperMethods.decodeToken();
+
+  function getConversations() {
+    console.log("HEY");
+    axios
+      .request({
+        method: "GET",
+        url: `/api/conversations/${userInfo.userID}`,
+      })
+      .then((result) => {
+        setConversations(result);
+      })
+      .catch((err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+  }
+  console.log(conversations);
+  useEffect(() => {
+    getConversations();
+  }, []);
   return <div className="messages-page">Messages will go here!</div>;
 }
 
