fb937b1ebc07bb23af528f35b41ba66500df9e81
Sarah Naas <sjnaas@gmail.com>
2024-02-14 00:01:03 +0100

    added reducer

--- client/src/components/MessagesPage/index.js
+++ client/src/components/MessagesPage/index.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
 import AuthHelperMethods from "../../helpers/AuthHelperMethods";
 import "./index.css";
 
-function MessagesPage() {
-  const [conversations, setConversations] = useState();
+function MessagesPage(props) {
+  const { conversations, loadConversations } = props;
+  const [messages, setMessages] = useState([]);
   const userInfo = AuthHelperMethods.decodeToken();
 
   function getConversations() {
@@ -14,7 +16,7 @@ function MessagesPage() {
         url: `/api/conversations/${userInfo.userID}`,
       })
       .then((result) => {
-        setConversations(result);
+        loadConversations(result.data);
       })
       .catch((err) => {
         if (err) {
@@ -28,14 +30,44 @@ function MessagesPage() {
   }, []);
   return (
     <div className="lending-page">
-      <div className="lending-books-div"></div>
+      <div className="lending-books-div">
+        {conversations?.map((convo) => {
+          console.log(convo);
+          const { name } = convo.participants.find((participant) => {
+            return participant.id !== userInfo.userID;
+          });
+          console.log(name);
+          return (
+            <button onClick={() => setMessages(convo.messages)}>{name}</button>
+          );
+        })}
+      </div>
       <div className="form">
         <div className="render-books">
-          <div className="book-container"></div>
+          <div className="book-container">
+            {messages.map((message) => {
+              return <div>{message.message}</div>;
+            })}
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default MessagesPage;
+// //reading out of the redux state into the component's props
+const mapStateToProps = (state) => {
+  return {
+    conversations: state.conversations.data,
+  };
+};
+
+//writing
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadConversations: (data) =>
+      dispatch({ type: "CONVERSATIONS_LOADED", data: data }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessagesPage);
