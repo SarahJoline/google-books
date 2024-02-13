fa25fa7f86d96ce7a7cc33aede67443af4ac5118
Sarah Naas <sjnaas@gmail.com>
2024-02-13 13:45:15 +0100

    created bookcard component

---
+++ client/src/components/Cards/BookCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../BorrowPage/index.css";
+
+function BookCard({
+  book,
+  bookID,
+  _id,
+  image,
+  title,
+  handleClick,
+  text,
+  open,
+  openModal,
+  loggedInStatus,
+  buttonClass,
+}) {
+  return (
+    <div className="book-card" key={_id}>
+      <img className="book-image" src={image} alt={title} />
+
+      <div className="button-div">
+        <button
+          className={buttonClass}
+          onClick={() => {
+            loggedInStatus && openModal
+              ? handleClick(book, open)
+              : loggedInStatus && !openModal
+              ? handleClick(book)
+              : console.log("Not logged in");
+          }}
+          id={bookID}
+          data={book}
+        >
+          {text}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default BookCard;
