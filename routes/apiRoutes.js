ad0b1fc88646c9e30add98e7e6b3e3d3651fc7bd
Sarah Naas <sjnaas@gmail.com>
2024-02-14 14:27:33 +0100

    created new message creation route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -190,20 +190,18 @@ router.delete("/books/delete/:id", (req, res) => {
     });
 });
 
-router.post("/messages/send", async (req, res) => {
+router.post("/conversations/send", async (req, res) => {
   const { book, userID, message, participants } = req.body;
 
   let conversationID;
   const conversation = await db.Conversation.findOne({
     participants: participants,
   });
-  console.log(conversation);
   if (!conversation) {
     await db.Conversation.create({
       participants: participants,
     })
       .then((res) => {
-        console.log(res);
         conversationID = res._id;
       })
       .catch((err) => {
@@ -225,6 +223,29 @@ router.post("/messages/send", async (req, res) => {
 
   db.Conversation.findOneAndUpdate(
     { _id: conversationID },
+    { $push: { messages: newMessage._id } }
+  )
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+});
+
+router.post("/conversation/message/send/:id", async (req, res) => {
+  const newMessage = await db.Message.create({
+    conversationID: req.params.id,
+    message: message,
+    senderID: userID,
+    userBookId: book._id,
+    participants: participants,
+  });
+
+  db.Conversation.findOneAndUpdate(
+    { _id: req.params.id },
     { $push: { messages: newMessage._id } }
   )
     .then((result) => {
